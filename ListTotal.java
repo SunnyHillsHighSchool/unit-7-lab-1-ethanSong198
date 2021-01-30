d19b9fecbbf035752aa821db3525dfcb52bdcb23
Ethan Song <600020389@fjuhsd.org>
2021-01-30 18:26:17 +0000

    Finished making method

--- ListTotal.java
+++ ListTotal.java
@@ -10,6 +10,17 @@ public class ListTotal
 {
  public static int total( List<Integer> ray )
  {
-  return 0;
+  //create variable called sum
+  int sum = 0;
+
+  //for each loop of array list with int x
+  for(int x: ray){
+    //have sum be sum plus x
+    sum = sum + x;
+    //end for loop
+  }
+
+  //return sum
+  return sum;
  }
 }
